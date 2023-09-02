9dddd60f2d13ae3fad92ba51e3ab273cd5f9b7be
Saw <ehkalanephaung96@gmail.com>
2023-09-02 14:45:21 +0700

    Check point 2.7 add 6 events

--- src/main/java/se331/lab/rest/controller/EventController.java
+++ src/main/java/se331/lab/rest/controller/EventController.java
@@ -36,6 +36,78 @@ public class EventController {
                 .petAllowed(true)
                 .organizer("Fern Pollin")
                 .build());
+        eventList.add(Event.builder()
+                .id(789L)
+                .category("music")
+                .title("Summer Music Festival")
+                .description("Enjoy a day of live music and fun in the sun.")
+                .location("Sunset Park")
+                .date("July 15, 2022")
+                .time("15:00")
+                .petAllowed(false)
+                .organizer("Melody Masters")
+                .build());
+
+        eventList.add(Event.builder()
+                .id(101L)
+                .category("sports")
+                .title("Charity Fun Run")
+                .description("Join us for a charity run to support a good cause.")
+                .location("Central Park")
+                .date("April 5, 2022")
+                .time("09:30")
+                .petAllowed(false)
+                .organizer("Run for Charity")
+                .build());
+
+        eventList.add(Event.builder()
+                .id(222L)
+                .category("arts")
+                .title("Art Exhibition Opening")
+                .description("Explore the works of talented local artists.")
+                .location("Gallery 123")
+                .date("May 20, 2022")
+                .time("18:00")
+                .petAllowed(false)
+                .organizer("Artistic Expressions")
+                .build());
+
+        eventList.add(Event.builder()
+                .id(333L)
+                .category("technology")
+                .title("Tech Conference 2022")
+                .description("Stay updated with the latest tech trends and innovations.")
+                .location("Tech Center")
+                .date("September 8, 2022")
+                .time("09:00")
+                .petAllowed(false)
+                .organizer("TechConnect")
+                .build());
+
+        eventList.add(Event.builder()
+                .id(555L)
+                .category("health")
+                .title("Fitness and Wellness Expo")
+                .description("Explore fitness and wellness products and services.")
+                .location("Wellness Arena")
+                .date("June 10, 2022")
+                .time("11:30")
+                .petAllowed(false)
+                .organizer("Wellness World")
+                .build());
+
+        eventList.add(Event.builder()
+                .id(777L)
+                .category("education")
+                .title("STEM Workshop for Kids")
+                .description("Engage kids in science, technology, engineering, and math activities.")
+                .location("KidsTech Lab")
+                .date("October 2, 2022")
+                .time("14:00")
+                .petAllowed(false)
+                .organizer("STEM Education Foundation")
+                .build());
+
 
     }
 }
